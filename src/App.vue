9544e0b17e1cc48c2a63572e9582fa06cabc75e9
王逢超 <wangfengchao@tbkt.cn>
2018-01-11 09:16:53 +0800

    * 应用 history 路由

--- src/App.vue
+++ src/App.vue
@@ -23,6 +23,7 @@
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     color: #2c3e50;
+    overflow: auto;
   }
   .clear::before {
     content: '';
--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,7 @@ import Test from 'CUSTOM/Test';
 Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
